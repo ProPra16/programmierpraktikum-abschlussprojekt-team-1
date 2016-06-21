8c0bece1c350c4d1e619862d5560f42cc36eefa8
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-21 17:45:16 +0200

    Grundgeruest GUI

--- TDD-Trainer/src/gui/Gui.java
+++ TDD-Trainer/src/gui/Gui.java
@@ -1,4 +1,40 @@
 package gui;
 
-public class Gui {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application{
+	public static void main(String[] args){
+		launch();
+	}
+	@Override
+	public void start(Stage stage){
+		Scene main_scene = create_scene();
+		stage.setScene(main_scene);
+		stage.show();
+	}
+	private Scene create_scene(){
+		BorderPane root = new BorderPane();
+		TabPane main = create_buttons_top(); //TODO: doofer name
+		root.setCenter(main);
+		Scene scene = new Scene(root,700,700); //TODO: groesse von bildschirmgroesse abhaengig machen
+		return scene;
+	}
+	private TabPane create_buttons_top(){
+		TabPane menue = new TabPane();
+		menue.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+		Tab code = new Tab("Code");
+		Tab test = new Tab("Tests");
+		Tab konsole = new Tab("Konsole");
+		code.setContent(null); //TODO
+		test.setContent(null); //TODO
+		konsole.setContent(null); //TODO
+		menue.getTabs().addAll(code, test, konsole);
+		return menue;
+	}
 }
