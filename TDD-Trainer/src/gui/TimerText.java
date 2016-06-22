79921521e410d5e90e17800af1f9515f6c6b5b8b
Lukas Rose <public@lrose.de>
2016-06-22 14:02:58 +0200

    Implemented Thread functionality in TimerText-Class

--- TDD-Trainer/src/gui/TimerText.java
+++ TDD-Trainer/src/gui/TimerText.java
@@ -3,5 +3,24 @@ package gui;
 import javafx.scene.text.Text;
 
 public class TimerText extends Text implements Runnable{
+	private long startMillis = 0L;
+	private int updateMillis = 100;
+	private Thread t;
+	private boolean running = false;
 	
+	public TimerText(){
+		super("Time: 0:0.0");
+	}
+	
+	public void run(){
+		startMillis = System.currentTimeMillis();
+		while (running){
+			update();
+			try {
+				Thread.sleep(updateMillis);
+			} catch (InterruptedException e) {
+				//DO Nothing
+			}
+		}
+	}
 }
