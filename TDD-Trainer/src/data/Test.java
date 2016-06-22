c2af302b85f6f956fdb6ed89f685878f00721b92
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-22 12:04:26 +0200

    Test-Objekt

---
+++ TDD-Trainer/src/data/Test.java
@@ -0,0 +1,11 @@
+package data;
+
+public class Test {
+	private String text;
+	public Test(String text){
+		this.text = text;
+	}
+	public void einlesen(){ //zum einlesen delimiter?
+		// TODO: implementieren
+	}
+}
