6cb3f68fd1862536ac10d085fc936337f1755b6f
Lukas Rose <public@lrose.de>
2016-06-22 15:49:51 +0200

    Implemented static final phase vars

--- TDD-Trainer/src/gui/Phase.java
+++ TDD-Trainer/src/gui/Phase.java
@@ -1,14 +1,20 @@
 package gui;
 
 public class Phase {
+	public static final int TESTS = 0;
+	public static final int CODE = 1;
+	public static final int REFACTOR = 2;
+	
 	private int phase;
 	public Phase(){
 		this.phase = 1;
 	}
+	
 	public void next_phase(){
-		this.phase = this.phase %4;
+		this.phase = this.phase %2;
 	}
-	public int get(){
-		return phase;
+	
+	public void set_phase(int phase){
+		this.phase = phase;
 	}
 }
