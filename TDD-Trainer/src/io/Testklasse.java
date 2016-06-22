4f21b0cff9e0c6177a12e98ce0e49d9d44d4010d
Lukas Rose <public@lrose.de>
2016-06-22 11:39:53 +0200

    Test-Tests

---
+++ TDD-Trainer/src/io/Testklasse.java
@@ -0,0 +1,15 @@
+package io;
+
+public class Testklasse {
+	public static String test(){
+		return "Banane";
+	}
+	
+	public static String foo(){
+		return "bar";
+	}
+	
+	public static String bar(){
+		return "foo";
+	}
+}
---
+++ TDD-Trainer/src/test/TestTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestTest {
+	@Test
+	public void testeTestklasse1(){
+		assertEquals("Banane",io.Testklasse.test());
+	}
+	
+	@Test
+	public void testeTestklasse2(){
+		assertEquals("foo", io.Testklasse.bar());
+	}
+	
+	@Test
+	public void testeTestklasse3(){
+		assertEquals("bar", io.Testklasse.foo());
+	}
+	
+	@Test
+	public void failingTestTestklasse(){
+		assertEquals("bar", io.Testklasse.bar());
+	}
+}
