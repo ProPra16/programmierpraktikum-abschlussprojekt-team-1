f290e76892f3a538ffe10838009135b40d54c1c2
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-22 11:03:51 +0200

    FileHandler - read_file()

--- TDD-Trainer/src/io/FileHandler.java
+++ TDD-Trainer/src/io/FileHandler.java
@@ -1,8 +1,12 @@
 package io;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileHandler {
 	public static void to_file(String string, String source){ //TODO: source als file
@@ -15,5 +19,8 @@ public class FileHandler {
 			e.printStackTrace();
 		}
 
+	}
+	public static String read_file(String source) throws FileNotFoundException,SecurityException, IOException{ 
+			return new String(Files.readAllBytes(Paths.get(source)));
 	}
 }
