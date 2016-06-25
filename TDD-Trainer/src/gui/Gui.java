ff2e08ce93fc5d92dfc82a44831a5f2e796e6c8a
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-25 23:58:46 +0200

    Unused Import removed

--- TDD-Trainer/src/gui/Gui.java
+++ TDD-Trainer/src/gui/Gui.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
