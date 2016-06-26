89b398cbf22d3df3b7c13ed4cd7db1a6e9f4bf51
Daniela Prigge <daniela.prigge@web.de>
2016-06-27 01:56:02 +0200

    alles funktioniert, es fehlt noch was passiert wenn man auf "laden" drückt

--- TDD-Trainer/src/gui/Gui.java
+++ TDD-Trainer/src/gui/Gui.java
@@ -23,8 +23,6 @@ public class Gui extends Application{
 	public void start(Stage stage){
 		Scene main_scene = create_scene();
 		stage.setScene(main_scene);
-		Katalog katalog = new Katalog();
-		katalog.show();
 		stage.show();
 		stage.setOnCloseRequest(e->{
 			timer.stop();
