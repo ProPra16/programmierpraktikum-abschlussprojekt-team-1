4e0c4be7a780fcd03b4b6ee6b83de555c0247cd1
Daniela Prigge <daniela.prigge@web.de>
2016-06-27 18:50:35 +0200

    Man kann nun Tabs hinzufügen, indem man auf plus klickt.

--- TDD-Trainer/src/gui/CodePane.java
+++ TDD-Trainer/src/gui/CodePane.java
@@ -1,11 +1,53 @@
 package gui;
 
+import java.util.Optional;
+
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.StackPane;
-public class CodePane extends StackPane{
-	private TextArea text;
+import javafx.scene.control.TextInputDialog;
+
+public class CodePane extends TabPane{
+	Tab plusTab;
+	int plusTabIndex = 0;
 	public CodePane(){
-		text = new TextArea();
-		getChildren().add(text);
+		super();
+		setOnMouseClicked(e->{
+			clickOnPlus();
+		});
+		addPlus();
+	}
+	
+	private void clickOnPlus(){
+		if(plusTab.isSelected()){
+			TextInputDialog dialog = new TextInputDialog();
+			dialog.setTitle("Class name Input Dialog");
+			dialog.setHeaderText("Select a class name");
+			dialog.setContentText("Please enter the class name here:");
+			Optional<String> result = dialog.showAndWait();
+			result.ifPresent(name -> {
+				getTabs().remove(plusTabIndex);
+				addClass(name);
+				plusTabIndex++;
+				addPlus();
+			});
+		}
+	}
+	
+	private void addClass(String className){
+		TextArea text = new TextArea();
+		Tab classTab = new Tab(className);
+		classTab.setOnClosed(e->{
+			plusTabIndex--;
+		});
+		classTab.setContent(text);
+		getTabs().addAll(classTab);
+	}
+	private void addPlus(){
+		plusTab = new Tab("+");
+		plusTab.setClosable(false);
+		getTabs().addAll(plusTab);
+		
+		
 	}
 }
