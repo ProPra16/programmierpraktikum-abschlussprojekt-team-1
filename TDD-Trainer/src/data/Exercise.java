993094a19e1407e83b03d55f05a6602ffa2d0a0f
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-28 16:29:24 +0200

    class and exercise

---
+++ TDD-Trainer/src/data/Exercise.java
@@ -0,0 +1,16 @@
+package data;
+
+import java.util.List;
+
+public class Exercise {
+	private List<AllTests> all_tests;
+	private List<Class> klasse;
+	private String description, name;
+	private boolean babysteps;
+	private int duration;
+	private boolean tracker;
+	
+	public Exercise(){
+		
+	}
+}
