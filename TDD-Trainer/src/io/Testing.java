e0636b06b9763fdfe71f4c4e64c5457ac803412a
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-29 11:54:42 +0200

    comments

--- TDD-Trainer/src/io/Testing.java
+++ TDD-Trainer/src/io/Testing.java
@@ -15,18 +15,24 @@ public class Testing {
 		if(hasCompileErrors(comp_un)){
 			List<CompileError> errors = getCompileErrors(comp_un);
 			for(CompileError error: errors){
-				console.set_text(error.toString());//TODO: .toString() - lieber selbst zusammenbasteln
+				console.set_text(error.toString());
+				//TODO: ueberlegen: .toString()? - lieber selbst zusammenbasteln?
 			}
 		}
 	}
+	
+	public static void test(){
+		
+	}
+	
 	public static boolean hasCompileErrors(CompilationUnit x){
 		CompilerResult y = (CompilerResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
 		return y.hasCompileErrors();
 	}
-	public static CompilerResult getCompileResult(CompilationUnit x){
+	public static CompilerResult getCompileResult(CompilationUnit x){ //TODO: braucht man das?
 		return (CompilerResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
 	}
-	public static List<CompileError> getCompileErrors(CompilationUnit x){
+	private static List<CompileError> getCompileErrors(CompilationUnit x){
 		CompilerResult y = (CompilerResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
 		return (List<CompileError>) y.getCompilerErrorsForCompilationUnit(x);
 	}
