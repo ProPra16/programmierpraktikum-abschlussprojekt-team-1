c214872a5db36e0ddcceb7238bc6941b6e47a707
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-29 09:08:38 +0200

    changes in class&test, testing-klasse

--- TDD-Trainer/src/data/Test.java
+++ TDD-Trainer/src/data/Test.java
@@ -1,5 +1,7 @@
 package data;
 
+import vk.core.api.CompilationUnit;
+
 public class Test {
 	private String code;
 	private String old_code;
@@ -13,5 +15,11 @@ public class Test {
 	}
 	public String getName(){
 		return name;
+	}
+	public CompilationUnit getCompilationUnit(){
+		return new CompilationUnit(name, code, true); //TODO: ein besserer name muss her
+	}
+	public void backToOldCode(){
+		code = old_code;
 	}
 }
