f3c0310f8acd47e1b4680b0ad5e322fd8f682aeb
Lukas <public@lrose.de>
2016-06-29 09:15:53 +0200

    Finished AlertHandler

--- TDD-Trainer/src/gui/AlertHandler.java
+++ TDD-Trainer/src/gui/AlertHandler.java
@@ -10,30 +10,26 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class AlertHandler {
-	public static void newProject_alert(){
+	public static final int LOAD_PROJECT = 2;
+	public static final int LOAD_TEMPLATE = 1;
+	public static final int NEW_PROJECT = 0;
+	public static int returnValue = NEW_PROJECT;
+	
+	public static int newProject_alert(){
 		Alert loadOrNew = new Alert(AlertType.CONFIRMATION);
 		loadOrNew.setHeaderText(null);
 		loadOrNew.setContentText("Load an exercise or create a new program?");
-		ButtonType load = new ButtonType("Load");
-		ButtonType newProgram = new ButtonType("New project");
-		loadOrNew.getButtonTypes().setAll(load, newProgram);
-		loadOrNew.showAndWait().ifPresent(event-> {
-			if(event == load){
-				loadOrNew.close();
-				Catalog katalog = new Catalog();
-				katalog.showAndWait();
-				if(katalog.wurdeGeladen()){
-//					stage.setScene(create_scene());
-//					stage.show();
-				}
-				
-			}
-			else if(event == newProgram){
-				babysteps_alert();
-//				stage.setScene(create_scene());
-//				stage.show();
-			}
+		ButtonType loadTemplate = new ButtonType("Load a template");
+		ButtonType loadProject = new ButtonType("Load existing project");
+		ButtonType newProject = new ButtonType("New project");
+		loadOrNew.getButtonTypes().setAll(loadTemplate, loadProject, newProject);
+		loadOrNew.showAndWait().ifPresent((event) -> {
+			if(event == loadTemplate) returnValue = LOAD_TEMPLATE;
+			else if(event == loadProject) returnValue = LOAD_PROJECT;
+			else if(event == newProject) returnValue = NEW_PROJECT;
+			
 		});
+		return returnValue;
 	}
 	
 	public static void babysteps_alert(){
@@ -47,13 +43,12 @@ public class AlertHandler {
 		alert.getButtonTypes().setAll(yes, no);
 		alert.showAndWait().ifPresent(event-> {
 			if(event == yes) ex.setBabysteps(true);
-			else if(event == no) ex.setBabysteps(false); //warum geht das ohne else ned??
-//			else Constants.getExercise().setBabysteps(false);
+			else if(event == no) ex.setBabysteps(false);
 		});
 		if(ex.getBabysteps()){
 			do {
 				TextInputDialog b_duration = new TextInputDialog();
-				b_duration.setContentText("How many secounds?"); //TODO: besseren text...
+				b_duration.setContentText("How many seconds?"); //TODO: besseren text...
 				b_duration.setHeaderText(null);
 				b_duration.showAndWait().ifPresent(input ->{
 					try{
