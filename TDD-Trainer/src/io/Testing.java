0ae86bccfec987b5b288d9e10c451ada4cfccb84
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-30 17:31:28 +0200

    Testing - changes

--- TDD-Trainer/src/io/Testing.java
+++ TDD-Trainer/src/io/Testing.java
@@ -1,13 +1,12 @@
 package io;
 
-import java.util.List;
-
-import org.junit.runner.JUnitCore;
-
+import java.util.Collection;
 import gui.ConsolePane;
 import vk.core.api.CompilationUnit;
 import vk.core.api.CompileError;
 import vk.core.api.CompilerResult;
+import vk.core.api.TestFailure;
+import vk.core.api.TestResult;
 import vk.core.internal.InternalResult;
 
 public class Testing {
@@ -15,27 +14,40 @@ public class Testing {
 	
 	public static void compile(CompilationUnit comp_un){ //gibt fehlermeldungen, wenn vorhanden auf die console aus
 		if(hasCompileErrors(comp_un)){
-			List<CompileError> errors = getCompileErrors(comp_un);
+			Collection<CompileError> errors = getCompileErrors(comp_un);
 			for(CompileError error: errors){
 				console.set_text(error.toString());
-				//TODO: ueberlegen: .toString()? -  lieber selbst zusammenbasteln?
+				//Reb: ueberlegen: .toString()? -  lieber selbst zusammenbasteln?
 			}
 		}
-	}
-	
-	public static void test(){
-		//TODO: soll testen
-	}
-	
-	public static boolean hasCompileErrors(CompilationUnit x){
-		CompilerResult y = (CompilerResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
+	}	
+	public static boolean hasCompileErrors(CompilationUnit comp_un){
+		CompilerResult y = getInternalResult(comp_un);
 		return y.hasCompileErrors();
 	}
-	public static CompilerResult getCompileResult(CompilationUnit x){ //TODO: braucht man das?
-		return (CompilerResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
+	public static InternalResult getInternalResult(CompilationUnit x){ //TODO: braucht man das?
+		return (InternalResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
 	}
-	private static List<CompileError> getCompileErrors(CompilationUnit x){
-		CompilerResult y = (CompilerResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
-		return (List<CompileError>) y.getCompilerErrorsForCompilationUnit(x);
+	private static Collection<CompileError> getCompileErrors(CompilationUnit comp_un){
+		CompilerResult y = getInternalResult(comp_un);
+		return y.getCompilerErrorsForCompilationUnit(comp_un);
+	}
+	public static boolean tests_ok(CompilationUnit comp_un){
+		TestResult test_res = getInternalResult(comp_un);
+		return(test_res.getNumberOfFailedTests() ==0);
+	}
+	public static void test(CompilationUnit comp_un){
+		InternalResult test_res = getInternalResult(comp_un); //Reb: besser als test_res, aber woher dann test-errors?
+		int tests_ok = test_res.getNumberOfSuccessfulTests();
+		int tests_fail = test_res.getNumberOfFailedTests();
+		Collection<TestFailure> fails = test_res.getTestFailures();
+		//TODO: ausgeben was expected war und was rausgekommen ist
+		console.set_text("OK: "+tests_ok+" FAIL: "+tests_fail);
+		for(TestFailure fail: fails){  //TODO: gucken was da ausgegeben wird und dann anpassen
+			console.set_text(fail.getTestClassName());
+			console.set_text(fail.getMethodName());
+			console.set_text(fail.getMessage());
+			console.set_text(fail.getExceptionStackTrace());
+		}
 	}
 }
