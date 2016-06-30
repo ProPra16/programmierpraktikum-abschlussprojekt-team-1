059757bcc49c54a680890f20a40421a9301208fb
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-30 18:40:23 +0200

    Removed more unnused stuff

--- TDD-Trainer/src/gui/Catalog.java
+++ TDD-Trainer/src/gui/Catalog.java
@@ -3,16 +3,12 @@ package gui;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
-import data.AllTests;
 import data.Class;
 import data.Project;
 import data.Test;
