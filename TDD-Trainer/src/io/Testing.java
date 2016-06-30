512deb869bd890b2ca7e655bd72e99def18d022e
R Wagner <Rebecca.Wagner@hhu.de>
2016-06-30 18:19:39 +0200

    Testing-Methoden implementiert (testen, tests_ok, comp., comp_ok)

--- TDD-Trainer/src/io/Testing.java
+++ TDD-Trainer/src/io/Testing.java
@@ -5,49 +5,62 @@ import gui.ConsolePane;
 import vk.core.api.CompilationUnit;
 import vk.core.api.CompileError;
 import vk.core.api.CompilerResult;
+import vk.core.api.JavaStringCompiler;
 import vk.core.api.TestFailure;
 import vk.core.api.TestResult;
-import vk.core.internal.InternalResult;
+import vk.core.internal.InternalCompiler;
 
 public class Testing {
 	private static ConsolePane console;
 	
-	public static void compile(CompilationUnit comp_un){ //gibt fehlermeldungen, wenn vorhanden auf die console aus
-		if(hasCompileErrors(comp_un)){
-			Collection<CompileError> errors = getCompileErrors(comp_un);
-			for(CompileError error: errors){
-				console.set_text(error.toString());
-				//Reb: ueberlegen: .toString()? -  lieber selbst zusammenbasteln?
+	public static void compile(CompilationUnit[] comp_uns){ //gibt fehlermeldungen, wenn vorhanden auf die console aus
+		JavaStringCompiler comp = getJSC(comp_uns);
+		CompilerResult comp_res = comp.getCompilerResult();
+		if(comp_res.hasCompileErrors()){
+			for(CompilationUnit cu: comp_uns){
+				Collection<CompileError> errors = comp_res.getCompilerErrorsForCompilationUnit(cu);
+				print_errors_to_console(errors);
 			}
 		}
-	}	
-	public static boolean hasCompileErrors(CompilationUnit comp_un){
-		CompilerResult y = getInternalResult(comp_un);
-		return y.hasCompileErrors();
 	}
-	public static InternalResult getInternalResult(CompilationUnit x){ //TODO: braucht man das?
-		return (InternalResult) new InternalResult().getCompilerErrorsForCompilationUnit(x);
+	
+	private static void print_errors_to_console(Collection<CompileError> errors){ //gibt compileerrors auf console aus
+		for(CompileError error: errors){
+			console.set_text(error.toString());
+			//Reb: ueberlegen: .toString()? -  lieber selbst zusammenbasteln?
+		}
 	}
-	private static Collection<CompileError> getCompileErrors(CompilationUnit comp_un){
-		CompilerResult y = getInternalResult(comp_un);
-		return y.getCompilerErrorsForCompilationUnit(comp_un);
+	
+	public static boolean hasCompileErrors(CompilationUnit[] comp_uns){ //fuer next-button
+		JavaStringCompiler comp = getJSC(comp_uns);
+		CompilerResult comp_res = comp.getCompilerResult();
+		return comp_res.hasCompileErrors();
 	}
-	public static boolean tests_ok(CompilationUnit comp_un){
-		TestResult test_res = getInternalResult(comp_un);
+	
+	public static boolean tests_passed(CompilationUnit[] comp_uns){ //fuer next-button
+		JavaStringCompiler comp = getJSC(comp_uns);
+		TestResult test_res = comp.getTestResult();
 		return(test_res.getNumberOfFailedTests() ==0);
 	}
-	public static void test(CompilationUnit comp_un){
-		InternalResult test_res = getInternalResult(comp_un); //Reb: besser als test_res, aber woher dann test-errors?
+	
+	public static void test(CompilationUnit[] comp_uns){ //gibt testergebnisse auf console aus
+		JavaStringCompiler comp = getJSC(comp_uns);
+		TestResult test_res = comp.getTestResult();
 		int tests_ok = test_res.getNumberOfSuccessfulTests();
 		int tests_fail = test_res.getNumberOfFailedTests();
 		Collection<TestFailure> fails = test_res.getTestFailures();
-		//TODO: ausgeben was expected war und was rausgekommen ist
 		console.set_text("OK: "+tests_ok+" FAIL: "+tests_fail);
-		for(TestFailure fail: fails){  //TODO: gucken was da ausgegeben wird und dann anpassen
+		for(TestFailure fail: fails){  //TODO: gucken was da ausgegeben wird und dann anpassen (von der formatierung her)
 			console.set_text(fail.getTestClassName());
 			console.set_text(fail.getMethodName());
 			console.set_text(fail.getMessage());
 			console.set_text(fail.getExceptionStackTrace());
 		}
+	}
+
+	private static JavaStringCompiler getJSC(CompilationUnit[] comp_uns){ //erzeugt JSC und ruftcompileAndRunTests() auf
+		JavaStringCompiler comp = new InternalCompiler(comp_uns);
+		comp.compileAndRunTests();
+		return comp;
 	}
 }
