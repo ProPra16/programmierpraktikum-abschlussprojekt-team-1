1c5004ac2df1c504de0e36d67ca11e90342600cb
Lukas <public@lrose.de>
2016-07-05 17:43:06 +0200

    path fixed again!

--- util/publish-javadoc.sh
+++ util/publish-javadoc.sh
@@ -12,7 +12,7 @@ ls -ahl gh-pages
 # Commit and Push the Changes
 cd gh-pages
 git rm -rf ./javadoc
-cp -Rf ../TDD-Trainer/doc/ .
+cp -Rf ../TDD-Trainer/doc/ ./
 git add -f .
 git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
