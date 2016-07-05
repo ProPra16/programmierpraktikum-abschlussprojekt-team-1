cd3432a77e80cd838f61f5dac429e01a6dd78a86
Lukas <public@lrose.de>
2016-07-05 16:05:15 +0200

    added javadoc publish script for travis

---
+++ util/publish-javadoc.sh
@@ -0,0 +1,19 @@
+# (c) Ben Limmer, 2013, released under cc-by 4.0 (https://creativecommons.org/licenses/by/4.0/)
+# source: https://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages 
+https://${GH_TOKEN}@github.com/ReadyTalk/swt-bling gh-pages > /dev/null
+
+# Commit and Push the Changes
+cd gh-pages
+git rm -rf ./javadoc
+cp -Rf $HOME/javadoc-latest ./javadoc
+git add -f .
+git commit -m "Lastest javadoc on successful travis build 
+$TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
+
