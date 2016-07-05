6e0a1d8c3c78fea14ab3e83b73a90c4a05d1064a
Lukas <public@lrose.de>
2016-07-05 16:58:51 +0200

    More debug output

--- util/publish-javadoc.sh
+++ util/publish-javadoc.sh
@@ -7,6 +7,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --branch=gh-pages https://${GH_TOKEN}@github.com/ProPra16/programmierpraktikum-abschlussprojekt-team-1
 ls -ahl
+ls -ahl programmierpraktikum-abschlussprojekt-team-1/
 
 # Commit and Push the Changes
 cd gh-pages
