299070de8420081179d38ced26f518aaf4c93e91
Lukas <public@lrose.de>
2016-07-05 17:20:16 +0200

    travis

--- util/publish-javadoc.sh
+++ util/publish-javadoc.sh
@@ -2,18 +2,17 @@
 # source: https://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
 # Get to the Travis build directory, configure git and clone the repo
-cd $HOME
+#cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --branch=gh-pages https://${GH_TOKEN}@github.com/ProPra16/programmierpraktikum-abschlussprojekt-team-1
+git clone --branch=gh-pages https://${GH_TOKEN}@github.com/ProPra16/programmierpraktikum-abschlussprojekt-team-1 gh-pages
 ls -ahl
-ls -ahl programmierpraktikum-abschlussprojekt-team-1/
-cd programmierpraktikum-abschlussprojekt-team-1/
+ls -ahl gh-pages
 
 # Commit and Push the Changes
 cd gh-pages
 git rm -rf ./javadoc
-cp -Rf $HOME/javadoc-latest ./javadoc
+cp -Rf ../TDD-Trainer/doc ./javadoc
 git add -f .
 git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
