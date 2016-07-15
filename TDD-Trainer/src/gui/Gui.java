679a66668f152bb52c5452cad1640c6b1d95c240
R Wagner <Rebecca.Wagner@hhu.de>
2016-07-15 10:52:44 +0200

    Sollte jetzt wieder gehen

--- TDD-Trainer/src/gui/Gui.java
+++ TDD-Trainer/src/gui/Gui.java
@@ -240,6 +240,7 @@ public class Gui extends Application{
 		next.setOnAction(e->{
 			Test test = ((Test)(project.getTestList().get(0)));
 			updateTestProject();
+			updateClassProject();
 			if(phase.get()==Phase.TESTS && test.getNewTestCount()==1){
 				if(project.testHasCompileErrors()){ 
 					phase.next();
@@ -251,12 +252,10 @@ public class Gui extends Application{
 				}
 			}
 			else if(phase.get() == Phase.CODE && project.tests_ok()){
-				updateClassProject();
 				phase.next();
 				setPhaseRefactor();
 			}
 			else if(phase.get() == Phase.REFACTOR && project.tests_ok()){
-				updateClassProject();
 				phase.next();
 				setPhaseTest();
 			}else{
